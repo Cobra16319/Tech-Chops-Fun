ec179a66e768333c9c664426519c20176d8f4134
Cobra <46206055+Cobra16319@users.noreply.github.com>
2024-04-16 19:31:04 -0400

    Create networking.tf
    Creating Networking

---
+++ networking.tf
@@ -0,0 +1,27 @@
+#vpc
+resource "aws_vpc" "this" {
+  cidr_block = "10.100.0.0/16"
+  tags = {
+    Name = "upgrad-vpc"
+  }
+}
+
+#public subnets
+resource "aws_subnet" "public1" {
+  vpc_id     = aws_vpc.this.id
+  cidr_block = "10.100.1.0/24"
+  availability_zone = "us-east-1a"
+
+  tags = {
+    Name = "upgrad-public-1"
+  }
+}
+resource "aws_subnet" "public2" {
+  vpc_id     = aws_vpc.this.id
+  cidr_block = "10.100.2.0/24"
+  availability_zone = "us-east-1b"
+
+  tags = {
+    Name = "upgrad-public-2"
+  }
+}
