cb908e985f6900ffe2653ea9a75d7eb0d6567897
Cobra <46206055+Cobra16319@users.noreply.github.com>
2024-04-16 20:45:59 -0400

    Update instances.tf

--- instances.tf
+++ instances.tf
@@ -2,7 +2,7 @@
 resource "aws_instance" "wordpress" {
   ami                         = "ami-053b0d53c279acc90"
   instance_type               = "t2.micro"
-  key_name                    = "pem"
+  key_name                    = "wiz-key"
   subnet_id                   = aws_subnet.public1.id
   security_groups             = [aws_security_group.allow_ssh.id]
   associate_public_ip_address = true
