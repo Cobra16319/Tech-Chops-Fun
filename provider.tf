1cfa74fc565e3e4d5b626ddb8052b0d33902ff54
Cobra <46206055+Cobra16319@users.noreply.github.com>
2024-04-16 20:35:49 -0400

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,9 +1,13 @@
-#AWS Provider
 terraform {
   required_providers {
     aws = {
-      source = "hashicorp/aws"
-      version = "5.10.0"
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
     }
   }
+}
+
+# Configure the AWS Provider
+provider "aws" {
+  region = "us-east-1"
 }
