ad514ab271ed36b3bdce924e2f229c7846e149ed
Commander_Cobra <gregawscsa@gmail.com>
2024-04-16 20:26:06 -0400

    updated backend

---
+++ backend.tf
@@ -0,0 +1,11 @@
+# Using a single workspace:
+terraform {
+  backend "remote" {
+    hostname = "app.terraform.io"
+    organization = "Vetsindevops-HQ"
+
+    workspaces {
+      name = "Tech-Chops-Fun"
+    }
+  }
+}i
